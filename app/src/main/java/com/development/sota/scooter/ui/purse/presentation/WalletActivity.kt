59193c57556c7504b56bdf2c932b6384ba22a617
litov74dev <levt382@gmail.com>
2021-04-12 04:07:25 +0500

    Commit msg...

---
+++ app/src/main/java/com/development/sota/scooter/ui/purse/presentation/WalletActivity.kt
@@ -0,0 +1,86 @@
+package com.development.sota.scooter.ui.purse.presentation
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import androidx.core.content.ContextCompat
+import com.development.sota.scooter.R
+import com.development.sota.scooter.databinding.ActivityPurseBinding
+import com.development.sota.scooter.ui.purse.presentation.fragments.upbalance.UpBalanceActivity
+import kotlinx.android.synthetic.main.activity_purse.view.*
+import moxy.MvpAppCompatActivity
+import moxy.MvpView
+import moxy.ktx.moxyPresenter
+
+
+interface WalletView: MvpView
+
+interface WalletActivityView
+
+class WalletActivity : MvpAppCompatActivity(), WalletView, WalletActivityView{
+
+    private val presenter by moxyPresenter { WalletPresenter(this) }
+
+    private var _binding: ActivityPurseBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        _binding = ActivityPurseBinding.inflate(layoutInflater)
+        binding.imageButtonDrivingsListBack.setOnClickListener{
+            onBackPressed()
+        }
+
+        var key: Int = 0
+        val fm = supportFragmentManager
+        fm.beginTransaction().replace(R.id.host, UpBalanceActivity()).commit()
+
+        binding.btnOpenUpBalance.setOnClickListener{
+            if(key != 0){
+                unCheck(binding.btnOpenCards)
+                unCheck(binding.btnOpenTransactions)
+                check(binding.btnOpenUpBalance)
+                key = 0
+                fm.beginTransaction().replace(R.id.host, UpBalanceActivity()).commit()
+
+            }
+        }
+        binding.btnOpenCards.setOnClickListener{
+            if(key != 1){
+                check(binding.btnOpenCards)
+                unCheck(binding.btnOpenTransactions)
+                unCheck(binding.btnOpenUpBalance)
+                key = 1
+                fm.beginTransaction().replace(R.id.host, UpBalanceActivity()).commit()
+
+            }
+        }
+        binding.btnOpenTransactions.setOnClickListener{
+            if(key != 2){
+                unCheck(binding.btnOpenCards)
+                check(binding.btnOpenTransactions)
+                unCheck(binding.btnOpenUpBalance)
+                key = 2
+                fm.beginTransaction().replace(R.id.host, UpBalanceActivity()).commit()
+
+            }
+        }
+
+
+    }
+
+    private fun unCheck(btn: Button){
+        btn.background =
+            ContextCompat.getDrawable(this, R.drawable.ic_gray_segment_corner)
+        btn.elevation = 0f
+    }
+
+    private fun check(btn: Button){
+        btn.background =
+            ContextCompat.getDrawable(this, R.drawable.ic_white_corner)
+        btn.elevation = 4f
+    }
+
+}
--- app/src/main/java/com/development/sota/scooter/ui/purse/presentation/fragments/cards/CardsPresenter.kt
+++ app/src/main/java/com/development/sota/scooter/ui/purse/presentation/fragments/cards/CardsPresenter.kt
@@ -2,17 +2,19 @@ package com.development.sota.scooter.ui.purse.presentation.fragments.cards
 
 import android.content.Context
 import com.development.sota.scooter.common.base.BasePresenter
-import com.development.sota.scooter.ui.profile.domain.ProfileInteractorImpl
-import com.development.sota.scooter.ui.purse.domain.PurseCardsInteractor
-import com.development.sota.scooter.ui.purse.domain.PurseCardsInteractorImpl
+import com.development.sota.scooter.ui.purse.domain.WalletCardsViewInteractor
+import com.development.sota.scooter.ui.purse.domain.WalletViewInteractor
 import moxy.MvpPresenter
 
-class CardsPresenter(val context: Context): MvpPresenter<CardsView>(), BasePresenter{
-    private val interactor: PurseCardsInteractor = PurseCardsInteractorImpl(this)
+class CardsPresenter(
+    private val context: Context
+) : MvpPresenter<CardsView>(), BasePresenter {
+
+    private val interactor: WalletViewInteractor = WalletCardsViewInteractor(this)
+
 
     override fun onDestroyCalled() {
-        interactor.disposeRequests()
+        TODO("Not yet implemented")
     }
-
 
 }
